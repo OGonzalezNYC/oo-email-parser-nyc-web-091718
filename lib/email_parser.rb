2bbacecebf6ff725204ce9ee7c5bc53964842bdb
OGonzalezNYC <OGonzalezNYC@Gmail.com>
2018-09-22 21:02:50 -0400

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,14 +6,16 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' '). The parse method should, additionally, only return unique emails.
 class EmailParser
-  attr_accessor :string_of_email_addresses
+#  attr_accessor :string_of_email_addresses
 
   def initialize(string_of_email_addresses)
-    @string_of_email_addresses = string_of_email_addresses
+     @string_of_email_addresses = string_of_email_addresses
   end
 
   def parse
-    @string_of_email_addresses.split(" ").collect do |email_address|
+    #  string_of_email_addresses.split(" ").collect do |email_address|
+    #Because there's an attr_accessor, the above commented-out line would also work (without the "@"), in lieu of the below line.
+      @string_of_email_addresses.split(" ").collect do |email_address|
       if email_address[-1] == ","
         email_address.chomp(",")
       else
